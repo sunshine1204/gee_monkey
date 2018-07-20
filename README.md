750dfb0a8e62f695fc6d976191e7f759e940de9b
Dongdong <kongdd.sysu@gmail.com>
2018-07-20 13:32:15 +1100

    update 0.1.1
    add background of `submitted-to-backend` tasks

--- README.md
+++ README.md
@@ -1,16 +1,34 @@
-# GEE_Tools
-Google Earth Engine tools
+# gee_monkey
 
-1. Batch execute GEE Export task
+Batch export Google Earth Engine (GEE) tasks with `Tampermonkey`.
 
-Tired of click run button in the browser when exporting data?  
-Tired of translate JavaScript into python, because of inconvenient batch exporting?  
-Tired of paste JavaScript into console?  
++ Tired of click GEE tasks run button in browser? 
++ Tired of translate `JavaScript` into `python`, because of `JavaScript` inconvenient batch exporting? 
++ Tired of paste JavaScript into console?  
 
 Tampermonkey can solve those problems.
-Here is a little script about tampermonkey, which is used to batch click run button and comfirm all the tasks in GEE (https://github.com/kongdd/GEE_Tools/blob/master/gee_monkey.js).
 
-What your need is: chrome+Tampermonkey, and new Tampermonkey script, paste the content of gee_monkey.js into the script, then F5 refresh GEE.
 
-Hope it works for you!
-Welcome to improve this little script.
+Tasks submitted to GEE have two kinds:  
+
+- `task submitted-to-backend`: just submitted and wait in the queue
+- `task running-on-backend`: submitted and running now
+
+
+## Functions
+- **rALL** : batch run all tasks
+- **cALL** : cancel all tasks
+- **cSUN** : only cancel submitted-to-backend tasks, and leave running-on-backend tasks to be continue
+
+
+## Installation
+
+You need `chrome` and [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) (firefox is also OK).
+
++ 1 Install [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) extension in `chrome` or `firefox`.
++ 2 Dashboard → New script → paste the script in [gee_monkey](https://github.com/kongdd/GEE_Tools/blob/master/gee_monkey.js) → F5 refresh GEE website.
+
+## Updates  
+
+* 2018-07-20   
+  1. `running-on-backend` task' background is set to skyblue to distinguish `submitted-to-backend` task.
--- gee_monkey.js
+++ gee_monkey.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         GEE_tools
+// @name         gee_monkey
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
+// @version      0.1.1
+// @description  Batch export Google Earth Engine task
 // @author       Dongdong Kong
 // @match        https://code.earthengine.google.com/*
 // @grant        none
@@ -10,9 +10,28 @@
 // @downloadURL  https://raw.githubusercontent.com/kongdd/GEE_Tools/master/gee_monkey.js
 // @require      http://code.jquery.com/jquery-1.12.4.min.js
 // ==/UserScript==
-(function() {
-    'use strict';
+'use strict';
 
+// https://stackoverflow.com/questions/23683439/gm-addstyle-equivalent-in-tampermonkey
+function GM_addStyle(css) {
+    const style = document.getElementById("GM_addStyleBy8626") || (function() {
+        const style = document.createElement('style');
+        style.type = 'text/css';
+        style.id = "GM_addStyleBy8626";
+        document.head.appendChild(style);
+        return style;
+    })();
+    const sheet = style.sheet;
+    sheet.insertRule(css, (sheet.rules || sheet.cssRules || []).length);
+}
+
+GM_addStyle('.task.running-on-backend .content { background-color: #bee5f4;}');
+GM_addStyle('.gee_monkey { ' +
+    'text-align:center;padding:0 2px; margin:0 2px 0 2px;border:1px solid; ' +
+    'display: inline-block; width:40px; ' +
+    'font-weight:bold }');
+
+(function() {
     /**  confirm all the export task list
     function confirm_all() {
         $$('.goog-buttonset-default.goog-buttonset-action').forEach(function(e) { e.click(); });
@@ -91,7 +110,8 @@
             confirm_all();
         }
     }
-    function cancel_submitted(){
+
+    function cancel_submitted() {
         var tasklist = document.getElementsByClassName('task submitted-to-backend');
         for (var i = 0; i < tasklist.length; i++) {
             tasklist[i].children[3].click(); //indicator
@@ -103,9 +123,9 @@
         // alert('hello world!');
         var x = $("div.user");
         //         var x = $('div.task:first')
-        var $Run_all = $('<a id="run_all" style="cursor:pointer;text-decoration:none;padding:0 5px;border:1px solid;">Run All</a>');
-        var $Cancel_all = $('<a id="Cancel_all" style="cursor:pointer;text-decoration:none;padding:0 5px;border:1px solid;">Cancel All</a>');
-        var $Cancel_submitted = $('<a id="Cancel_all" style="cursor:pointer;text-decoration:none;padding:0 5px;border:1px solid;">Cancel subm</a>');
+        var $Run_all = $('<a class="gee_monkey" id="run_all" style="color:green">rAll</a>');
+        var $Cancel_all = $('<a class="gee_monkey" id="Cancel_all", style="color:red">cAll</a>');
+        var $Cancel_submitted = $('<a class="gee_monkey" id="Cancel_all", style="color:Purple">cSUB</a>');
 
         $Run_all.click(function() {
             //alert('Run_all 2');
